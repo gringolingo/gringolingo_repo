af79457510884c95dd5711c90ebc18bc9611df90
gringo lingo <gringolingoau@gmail.com>
2011-06-02 15:44:18 +1000

    adding a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gringolingo::Application.config.secret_token = '2126923e187c6ad6556cd4d707934cd9311c94f5870b2dd5712d869036c3d148690aff94961543b5551bca4ff18c087925f012a322e699156cd389dfbfd3ec81'
